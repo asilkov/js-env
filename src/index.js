3027aeef984de3e6fda5a47b6fc205dfa6b0d9f9
Azzi Silkov <azzi.silkov@gmail.com>
2018-09-04 21:51:35 -0700

    lint and test setup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import numeral from 'numeral';
 import './index.css'
 
 const courseVal = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseVal} for this.`);
+console.log(`I would pay ${courseVal} for this.`);//eslint-disable-line no-console
