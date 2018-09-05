c5312af5662dee5443770ad52aeadc5cbc1701bf
Azzi Silkov <azzi.silkov@gmail.com>
2018-09-04 17:46:50 -0700

    bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import { join } from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.dev'
 
 const port = 3000;
 const app = express();
+const compiler = webpack(webpackConfig);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: webpackConfig.output.publicPath
+}));
 
 app.get('/', function(req, res){
   res.sendFile(join(__dirname, '../src/index.html'));
